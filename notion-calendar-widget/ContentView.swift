d57eb38de6f251b75e960bbda7f5474caeca03c9
Giovanni <giovanni.caberlotto@igsolutions.it>
2024-08-04 14:29:21 +0200

    Initial Commit

---
+++ notion-calendar-widget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  notion-calendar-widget
+//
+//  Created by caber on 04/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ notion-calendar-widget/notion_calendar_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  notion_calendar_widgetApp.swift
+//  notion-calendar-widget
+//
+//  Created by caber on 04/08/24.
+//
+
+import SwiftUI
+
+@main
+struct notion_calendar_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
