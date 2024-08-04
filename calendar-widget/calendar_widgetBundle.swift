ddf8c5a814097b8755bad7184cc87c1632369e94
Giovanni <giovanni.caberlotto@igsolutions.it>
2024-08-04 18:19:11 +0200

    Init

---
+++ calendar-widget/calendar_widgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  calendar_widgetBundle.swift
+//  calendar-widget
+//
+//  Created by caber on 04/08/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct calendar_widgetBundle: WidgetBundle {
+    var body: some Widget {
+        calendar_widget()
+    }
+}
